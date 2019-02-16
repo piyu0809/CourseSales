ff11fd2c4b3687e28c157536c66f5ebfda8d7cba
piyu0809 <phirema1@binghamton.edu>
2019-02-15 19:24:58 -0500

    Updates

---
+++ precache-manifest.e904cfb836e38ccf3765a1adc5231143.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "10ce5b1090f48a5fb637",
+    "url": "/CourseSales/static/css/main.89542bdc.chunk.css"
+  },
+  {
+    "revision": "10ce5b1090f48a5fb637",
+    "url": "/CourseSales/static/js/main.10ce5b10.chunk.js"
+  },
+  {
+    "revision": "0ac8a0afa03a46467feb",
+    "url": "/CourseSales/static/js/1.0ac8a0af.chunk.js"
+  },
+  {
+    "revision": "2b7001ddeb2560f6db90",
+    "url": "/CourseSales/static/js/runtime~main.2b7001dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CourseSales/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0bb6933f967e437fabe0928a3a7082ca",
+    "url": "/CourseSales/static/media/checkout.0bb6933f.png"
+  },
+  {
+    "revision": "2a5c748e06a090760d19e4edb2f9317b",
+    "url": "/CourseSales/static/media/abc.2a5c748e.jpg"
+  },
+  {
+    "revision": "4882047999624b11b0576883ea6fb915",
+    "url": "/CourseSales/index.html"
+  }
+];
